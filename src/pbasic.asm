6ea9ef7140535b9b00292f485e8de34096f8eecc
Carlos Rabelo <developer@carlosrabelo.com.br>
2014-03-05 08:15:00 -0400

    feat: match BASIC keywords into one-byte tokens

--- src/pbasic.asm
+++ src/pbasic.asm
@@ -34,6 +34,28 @@
     # --- Static Strings ---
     STR_PROMPT:     .asciiz "> "
     STR_CRLF:       .asciiz "\n"
+    MSG_BANNER:     .asciiz "PBasic\n"
+    MSG_OK:         .asciiz "OK\n"
+    MSG_ERROR:      .asciiz "?SYNTAX ERROR\n"
+    MSG_NO_PROGRAM: .asciiz "?NO PROGRAM\n"
+    MSG_OUT_OF_MEMORY: .asciiz "?OUT OF MEMORY\n"
+    KW_LET:         .asciiz "LET"
+    KW_GOTO:        .asciiz "GOTO"
+    KW_GOSUB:       .asciiz "GOSUB"
+    KW_PRINT:       .asciiz "PRINT"
+    KW_IF:          .asciiz "IF"
+    KW_INPUT:       .asciiz "INPUT"
+    KW_RETURN:      .asciiz "RETURN"
+    KW_END:         .asciiz "END"
+    KW_LIST:        .asciiz "LIST"
+    KW_RUN:         .asciiz "RUN"
+    KW_NEW:         .asciiz "NEW"
+    KW_EXIT:        .asciiz "EXIT"
+    KW_REM:         .asciiz "REM"
+    KW_THEN:        .asciiz "THEN"
+    KW_FREE:        .asciiz "FREE"
+    KW_RND:         .asciiz "RND"
+    KW_ABS:         .asciiz "ABS"
 
 .text
 .globl main
@@ -115,10 +137,195 @@ TOK_STRING:
     j       TOK_LOOP
 
 TOK_LETTER:
+    addiu   $sp, $sp, -16
+    sw      $ra, 12($sp)
+    sw      $t0, 8($sp)
+    sw      $t1, 4($sp)
+
+    lw      $a0, 8($sp)
+    la      $a1, KW_PRINT
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_PRINT_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_LET
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_LET_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_IF
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_IF_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_GOTO
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_GOTO_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_GOSUB
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_GOSUB_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_INPUT
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_INPUT_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_RETURN
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_RETURN_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_THEN
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_THEN_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_END
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_END_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_REM
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_REM_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_LIST
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_LIST_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_RUN
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_RUN_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_NEW
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_NEW_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_EXIT
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_EXIT_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_FREE
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_FREE_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_RND
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_RND_MATCH
+    lw      $a0, 8($sp)
+    la      $a1, KW_ABS
+    jal     MATCH_KEYWORD
+    bnez    $v0, TK_ABS_MATCH
+
+    lw      $t1, 4($sp)
+    lw      $t0, 8($sp)
+    lw      $ra, 12($sp)
+    addiu   $sp, $sp, 16
+    lb      $t2, 0($t0)
     sb      $t2, 0($t1)
     addiu   $t0, $t0, 1
     addiu   $t1, $t1, 1
     j       TOK_LOOP
+
+TK_PRINT_MATCH:
+    li      $t3, 0x83
+    j       TK_KW_STORE
+TK_LET_MATCH:
+    li      $t3, 0x80
+    j       TK_KW_STORE
+TK_IF_MATCH:
+    li      $t3, 0x84
+    j       TK_KW_STORE
+TK_GOTO_MATCH:
+    li      $t3, 0x81
+    j       TK_KW_STORE
+TK_GOSUB_MATCH:
+    li      $t3, 0x82
+    j       TK_KW_STORE
+TK_INPUT_MATCH:
+    li      $t3, 0x85
+    j       TK_KW_STORE
+TK_RETURN_MATCH:
+    li      $t3, 0x86
+    j       TK_KW_STORE
+TK_THEN_MATCH:
+    li      $t3, 0x8D
+    j       TK_KW_STORE
+TK_END_MATCH:
+    li      $t3, 0x87
+    j       TK_KW_STORE
+TK_REM_MATCH:
+    lw      $t1, 4($sp)
+    lw      $ra, 12($sp)
+    addiu   $sp, $sp, 16
+    li      $t3, 0x8C
+    sb      $t3, 0($t1)
+    addiu   $t1, $t1, 1
+    move    $t0, $v1
+TK_REM_COPY_LOOP:
+    lbu     $t2, 0($t0)
+    sb      $t2, 0($t1)
+    beqz    $t2, TOK_DONE
+    addiu   $t0, $t0, 1
+    addiu   $t1, $t1, 1
+    j       TK_REM_COPY_LOOP
+TK_LIST_MATCH:
+    li      $t3, 0x88
+    j       TK_KW_STORE
+TK_RUN_MATCH:
+    li      $t3, 0x89
+    j       TK_KW_STORE
+TK_NEW_MATCH:
+    li      $t3, 0x8A
+    j       TK_KW_STORE
+TK_EXIT_MATCH:
+    li      $t3, 0x8B
+    j       TK_KW_STORE
+TK_FREE_MATCH:
+    li      $t3, 0xA0
+    j       TK_KW_STORE
+TK_RND_MATCH:
+    li      $t3, 0xA1
+    j       TK_KW_STORE
+TK_ABS_MATCH:
+    li      $t3, 0xA2
+    j       TK_KW_STORE
+
+TK_KW_STORE:
+    lw      $t1, 4($sp)
+    lw      $t0, 8($sp)
+    lw      $ra, 12($sp)
+    addiu   $sp, $sp, 16
+    move    $t0, $v1
+    sb      $t3, 0($t1)
+    addiu   $t1, $t1, 1
+    j       TOK_LOOP
+
+# -----------------------------------------------------------------------
+# MATCH_KEYWORD - Match keyword at $a1 against input at $a0
+# -----------------------------------------------------------------------
+MATCH_KEYWORD:
+    move    $t0, $a0
+    move    $t1, $a1
+MK_LOOP:
+    lb      $t3, 0($t1)
+    beqz    $t3, MK_END_CHECK
+    lb      $t2, 0($t0)
+    bne     $t2, $t3, MK_FAIL
+    addiu   $t0, $t0, 1
+    addiu   $t1, $t1, 1
+    j       MK_LOOP
+MK_END_CHECK:
+    lb      $t2, 0($t0)
+    beqz    $t2, MK_SUCCESS
+    li      $t3, 65
+    slt     $t3, $t2, $t3
+    bne     $t3, $zero, MK_SUCCESS
+    li      $t3, 90
+    slt     $t3, $t3, $t2
+    bne     $t3, $zero, MK_SUCCESS
+    j       MK_FAIL
+MK_FAIL:
+    li      $v0, 0
+    jr      $ra
+MK_SUCCESS:
+    li      $v0, 1
+    move    $v1, $t0
+    jr      $ra
 
 # -----------------------------------------------------------------------
 # INCHAR - Read a single character (syscall 12)
