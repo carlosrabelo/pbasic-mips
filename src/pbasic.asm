203ad9d2c3eb4ca019ce8568a1958147d5efd607
Carlos Rabelo <developer@carlosrabelo.com.br>
2014-03-13 08:54:00 -0400

    feat: clear program and variables with NEW

--- src/pbasic.asm
+++ src/pbasic.asm
@@ -1638,7 +1638,7 @@ DO_LIST:
     addiu   $sp, $sp, 4
     j       REPL
 DO_RUN:
-DO_NEW:
+# DO_NEW is provided by the NEW command checkbox.
 DO_EXIT:
 DO_REM:
     j       REPL
@@ -1823,3 +1823,30 @@ DL_ERR:
     lw      $ra, 12($sp)
     addiu   $sp, $sp, 16
     j       REPL_SYNTAX_ERROR
+# cmd_new.asm - NEW command execution (MIPS)
+# -----------------------------------------------------------------------
+
+.text
+
+# -----------------------------------------------------------------------
+# DO_NEW - Clears program memory and resets all variables
+# Input:  None
+# Output: None
+# Clobbers: None
+# -----------------------------------------------------------------------
+DO_NEW:
+    addiu   $sp, $sp, -4
+    sw      $ra, 0($sp)
+
+    jal     PROG_INIT
+    jal     VAR_INIT
+    
+    # Reset GOSUB stack pointer depth
+    la      $t0, MEM_GOSUB_SP
+    sw      $zero, 0($t0)
+
+    jal     PRINT_OK
+
+    lw      $ra, 0($sp)
+    addiu   $sp, $sp, 4
+    j       REPL
