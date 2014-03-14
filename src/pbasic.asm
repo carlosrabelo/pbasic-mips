ce0e1c851fb6e30978393b83d892f5fbcfc2d639
Carlos Rabelo <developer@carlosrabelo.com.br>
2014-03-14 09:48:00 -0400

    feat: jump to a stored line with GOTO

--- src/pbasic.asm
+++ src/pbasic.asm
@@ -1638,7 +1638,7 @@ RD_NOT_FREE:
 
 # Stubs replaced by later command checkboxes
 # DO_LET is provided by the LET command checkbox.
-DO_GOTO:
+# DO_GOTO is provided by the GOTO command checkbox.
 DO_GOSUB:
 DO_IF:
 DO_INPUT:
@@ -2073,3 +2073,46 @@ DO_END:
 DO_EXIT:
     addiu   $v0, $zero, 10      # Exit syscall code
     syscall
+# cmd_goto.asm - GOTO command execution (MIPS)
+# -----------------------------------------------------------------------
+
+.text
+
+# -----------------------------------------------------------------------
+# DO_GOTO - Jumps execution to a specific line number
+# Input:  None (uses MEM_TOKEN_PTR)
+# Output: None
+# Clobbers: None
+# -----------------------------------------------------------------------
+DO_GOTO:
+    addiu   $sp, $sp, -4
+    sw      $ra, 0($sp)
+
+    jal     EVAL_EXPR           # Get target line number in $v0
+    
+    addu    $a0, $v0, $zero     # Target line number in $a0
+    jal     LINE_FIND           # Find line, returns $v0=found(1/0), $v1=node address
+    beqz    $v0, DG_ERR         # If not found, syntax error
+
+    # Enable run flag
+    addiu   $t0, $zero, 1
+    la      $t1, MEM_RUN_FLAG
+    sw      $t0, 0($t1)
+
+    # Update MEM_LINE_PTR to the target line node
+    la      $t0, MEM_LINE_PTR
+    sw      $v1, 0($t0)
+
+    # Set MEM_TOKEN_PTR to point to the tokens of the line node ($v1 + 6)
+    addiu   $v1, $v1, 6
+    la      $t0, MEM_TOKEN_PTR
+    sw      $v1, 0($t0)
+
+    lw      $ra, 0($sp)
+    addiu   $sp, $sp, 4
+    j       REPL_DISPATCH
+
+DG_ERR:
+    lw      $ra, 0($sp)
+    addiu   $sp, $sp, 4
+    j       REPL_SYNTAX_ERROR
